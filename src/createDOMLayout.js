50270becc525cdcf5fc65e82d7c8df1d673c1fdf
ahad abdul <ahad.a2703@gmail.com>
2022-06-19 00:27:56 +1000

    added projects bar layout

--- src/createDOMLayout.js
+++ src/createDOMLayout.js
@@ -1,20 +1,64 @@
-const addToContent = (type, element) => {
-  const content = document.querySelector('.container');
-  content.insertAdjacentHTML(type, element);
+// --- OTHER FUNCTIONS ---
+const addToContent = (type, element, parent) => {
+  parent.insertAdjacentHTML(type, element);
 };
 
+// --- GET ELEMENET ---
+const getProjectsContainerElement = () => {
+  const element = document.querySelector('.projects');
+
+  return element;
+};
+
+const getProjectList = () => {
+  const element = document.querySelector('.project-list');
+
+  return element;
+}
+
+// -- NAV ELEMENT --
 const createNavElement = () => {
+  const content = document.querySelector('.container');
   const navHeader =
-`<nav class = "nav-header">
+  `<nav class = "nav-header">
     <h2 class = "title">Todo List</h2>
     <div class = "date-header">DATE</div>
   </nav>`;
 
-  addToContent('afterbegin', navHeader);
+  addToContent('afterbegin', navHeader, content);
 };
+
+// -- PROJECT ELEMENT --
+const createProjectHeader = () => {
+  const projectHeader =
+  `<h3 class = "project-header">Projects</h3>
+  <div class = "divider"></div>`;
+
+  addToContent('beforeend', projectHeader, getProjectsContainerElement());
+};
+
+const createProjectList = () => {
+  const projectList =
+  '<div class = "project-list"></div';
+
+  addToContent('beforeend', projectList, getProjectsContainerElement());
+};
+
+const addToProjectList = (projectName) => {
+  const newProject =
+  `<div class = "project-item">
+    <p class = "project">${projectName}</p>
+  </div>`;
+
+  addToContent('beforeend', newProject, getProjectList());
+};
+
+
 
 const createDOMLayout = () => {
   createNavElement();
+  createProjectHeader();
+  createProjectList();
 };
 
 export { createDOMLayout }
